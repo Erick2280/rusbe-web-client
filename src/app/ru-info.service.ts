9fa1d63cd46a600c60aa3ba8cf6f2a7a63fffe00
Erick Almeida <erickingles@gmail.com>
2019-08-15 23:46:07 -0300

    Implementando consumo da API do RU

--- src/app/ru-info.service.ts
+++ src/app/ru-info.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Storage } from '@ionic/storage';
+import * as environment from './environment.json';
 import * as moment from 'moment';
 import 'moment/locale/pt-br';
 
@@ -12,7 +13,7 @@ export class RuInfoService {
   constructor(private http: HttpClient, private storage: Storage) { }
 
   getTimeData() {
-    let isOpen: boolean;
+    let isOpen: boolean = false;
     let expectedOperation = 'none';
     const ruTimes = {
       startDesjejum: moment('07:00', 'hh:mm'),
@@ -22,16 +23,17 @@ export class RuInfoService {
       startJantar: moment('17:00', 'hh:mm'),
       endJantar: moment('19:00', 'hh:mm')
     };
-
-    if (moment().isBefore(ruTimes.endDesjejum)) {
-      expectedOperation = 'desjejum';
-      isOpen = moment().isAfter(ruTimes.startDesjejum);
-    } else if (moment().isBefore(ruTimes.endAlmoco)) {
-      expectedOperation = 'almoco';
-      isOpen = moment().isAfter(ruTimes.startAlmoco);
-    } else if (moment().isBefore(ruTimes.endJantar)) {
-      expectedOperation = 'jantar';
-      isOpen = moment().isAfter(ruTimes.startJantar);
+    if (!(moment().weekday(5) || moment().weekday(6))) {
+      if (moment().isBefore(ruTimes.endDesjejum)) {
+        expectedOperation = 'desjejum';
+        isOpen = moment().isAfter(ruTimes.startDesjejum);
+      } else if (moment().isBefore(ruTimes.endAlmoco)) {
+        expectedOperation = 'almoco';
+        isOpen = moment().isAfter(ruTimes.startAlmoco);
+      } else if (moment().isBefore(ruTimes.endJantar)) {
+        expectedOperation = 'jantar';
+        isOpen = moment().isAfter(ruTimes.startJantar);
+      }
     }
     const data = {
       day: moment().format('L'),
@@ -47,10 +49,10 @@ export class RuInfoService {
     // verifica se há dado na api, se houver ele mostra disponibilidade, se não, se for dia da semana ele diz que está disponível
     // se o dado for do dia, não puxar novamente
     return new Promise(resolve => {
-      // headers = headers.append('appid', '54f28c840f29309ea1af2802');
-      // headers = headers.append('secret', '7fad38e3638028445b27e883f7265614ad0346a852a68489c23f0f3906471fe6');
-      let dayToGet = '15/08/2019'.replace('/', '%2F'); // data.day.replace('/', '%2F');
-      this.http.get(`https://virtus.ufpe.br/api/v1.0/cardapio/dia?data=${dayToGet}`).subscribe(data => {
+
+      let dayToGet = '17/08/2019'.replace(/\//ig, '%2F');
+
+      this.http.get(`${environment.virtusApiUrl}?data=${dayToGet}`).subscribe(data => {
           resolve(data);
           console.log('ru status determined');
           console.log(data)
